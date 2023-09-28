12e3e9b0d45703a2217d12008b4d6307638a30d0
Tina Chen <tinachen@GinnywudeMacBook-Pro.local>
2023-09-28 10:05:30 +0800

    feat: add to top btn

--- js/all.js
+++ js/all.js
@@ -30,15 +30,43 @@ $(document).ready(function () {
     
 });
 
-// toTop BTN
-$(document).ready(function () {
-  $("#toTop").click(function (e) { 
-      e.preventDefault();
-      $("html,body").animate({
-          scrollTop:0
-      },200);
+
+
+// ToTop BTN
+const backToTopBtn = document.getElementById('backToTopBtn');
+
+  backToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   });
-});
 
-// Owl Carousel
+// ToTop BTN 換顏色
+// Function to change button color when scrolling
+function updateButtonColor() {
+  if (window.scrollY > 1800) {
+    backToTopBtn.classList.remove('bg-blue-300', 'text-white');
+    backToTopBtn.classList.add('bg-white/50', 'text-white'); // Replace with desired color classes
+  } else {
+    backToTopBtn.classList.remove('bg-white/50', 'text-white');
+    backToTopBtn.classList.add('bg-blue-300', 'text-white'); // Replace with initial color classes
+  }
+}
 
+// Call the function on page load and when the user scrolls
+updateButtonColor();
+window.addEventListener('scroll', updateButtonColor);
+
+// 顯示狀態
+// Function to show/hide the button with a transition effect based on scroll position
+function toggleButtonVisibility() {
+  if (window.scrollY > 200) {
+    backToTopBtn.classList.remove('hidden');
+    backToTopBtn.classList.add('opacity-100');
+  } else {
+    backToTopBtn.classList.remove('opacity-100');
+    // backToTopBtn.classList.add('hidden');
+  }
+}
+
+// Call the function on page load and when the user scrolls
+toggleButtonVisibility();
+window.addEventListener('scroll', toggleButtonVisibility);
